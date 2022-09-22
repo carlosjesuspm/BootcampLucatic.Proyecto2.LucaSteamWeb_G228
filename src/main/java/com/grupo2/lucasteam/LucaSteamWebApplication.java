83b8b072384752eb123eadf6fa7629e909fd25e0
Alonso <alonsogomezcasanova@gmail.com>
2022-09-22 17:24:39 +0200

    Proyecto creado

---
+++ src/main/java/com/grupo2/lucasteam/LucaSteamWebApplication.java
@@ -0,0 +1,13 @@
+package com.grupo2.lucasteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucaSteamWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucaSteamWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grupo2/lucasteam/LucaSteamWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo2.lucasteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaSteamWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
