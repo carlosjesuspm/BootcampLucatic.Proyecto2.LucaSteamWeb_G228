18c0715426b13aee53520bae4b91826a4877a42d
Lamia <99553152+Lhamia@users.noreply.github.com>
2022-09-23 09:18:26 +0200

    JuegosController+endpointListaJuegos

---
+++ src/main/java/com/grupo2/lucasteam/controller/JuegosController.java
@@ -0,0 +1,38 @@
+package com.grupo2.lucasteam.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+
+@Controller
+public class JuegosController {
+
+	@Autowired
+	JuegoService service;
+
+	private static final Logger log = LoggerFactory.getLogger(JuegosController.class);
+	
+	/** Metodo listaJuegos() - mapea la lista de juegos
+	* @param Listar juegos
+	* @return /endpoint listaJuegos
+	* @author Lamia
+	* @version 1.0 23/09/22
+	*/
+
+	@GetMapping("/")
+	public String listaJuegos(Model m) {
+		m.addAttribute("listaJuegos", service.findAll());
+
+		return "listaJuegos";
+
+	}
+	
+
+
+}
