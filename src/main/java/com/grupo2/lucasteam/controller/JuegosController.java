7d528c27e1273cfa078ab9df725bb8fbd89d9781
Lamia <99553152+Lhamia@users.noreply.github.com>
2022-09-23 09:41:32 +0200

    metodo-eliminarJuego(juegosController)

--- src/main/java/com/grupo2/lucasteam/controller/JuegosController.java
+++ src/main/java/com/grupo2/lucasteam/controller/JuegosController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 
@@ -33,6 +34,20 @@ public class JuegosController {
 
 	}
 	
+	
+	/** Metodo eliminarJuego() - permite eliminar un juego de la lista Juegos
+	* @param Eliminar juego
+	* @return /endpoint listaJuegos
+	* @author Lamia
+	* @version 1.0 23/09/22
+	*/
+	
+	@GetMapping("/eliminarJuego")
+	public String eliminarJuego(@RequestParam("id")int id) {
+		service.deleteById(id);
+		return ("redirect:/listaJuegos");
+		
+	}
 
 
 }
