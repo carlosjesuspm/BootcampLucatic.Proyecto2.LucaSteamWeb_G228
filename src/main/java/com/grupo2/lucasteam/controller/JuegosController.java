9b7dfaf6746e8bf9574f35f232b415169105435b
Alvaro-R <67501790+Alvaro-R@users.noreply.github.com>
2022-09-23 14:05:55 +0200

    correcion test model

--- src/main/java/com/grupo2/lucasteam/controller/JuegosController.java
+++ src/main/java/com/grupo2/lucasteam/controller/JuegosController.java
@@ -1,21 +1,21 @@
 package com.grupo2.lucasteam.controller;
 
-
-import java.sql.Date;
-import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
-import java.util.List;
-
-
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.grupo2.lucasteam.model.Editor;
+import com.grupo2.lucasteam.model.Genero;
+import com.grupo2.lucasteam.model.Juego;
+import com.grupo2.lucasteam.model.Plataforma;
 import com.grupo2.lucasteam.service.JuegosServiceI;
 
 /**
@@ -45,11 +45,12 @@ public class JuegosController {
 	/**
 	 * Descripción del método: Metodo que permite obtener un listado de todos los
 	 * juegos para que terminen viéndose en la interfaz creada para ello
-	* @return {@code listaJuegos} 
-	* @author Carlos Jesús Pérez Márquez
-	* @since 1.0
-	*/
-	
+	 * 
+	 * @return {@code listaJuegos}
+	 * @author Carlos Jesús Pérez Márquez
+	 * @since 1.0
+	 */
+
 	@GetMapping("/")
 	public String listaJuegos(Model m) {
 //		m.addAttribute("listaJuegos", service.findAll());
@@ -71,36 +72,39 @@ public class JuegosController {
 		}
 		m.addAttribute("listaJuegos", juegos);
 //		datos prueba
-		
+
 		return "listaJuegos";
 	}
 
+	/**
+	 * Metodo modificarJuego() - permite modificar un juego de la lista Juegos
+	 * 
+	 * @param Modificar juego
+	 * @return /endpoint formularioJuego
+	 * @author Lamia
+	 * @version 1.0 23/09/22
+	 */
 
-	/** Metodo modificarJuego() - permite modificar un juego de la lista Juegos
-	* @param Modificar juego
-	* @return /endpoint formularioJuego
-	* @author Lamia
-	* @version 1.0 23/09/22
-	*/
-	
 	@GetMapping("/modificarJuego")
-	public String modificarJuego(@RequestParam("id")int id, Model m) {
+	public String modificarJuego(@RequestParam("id") int id, Model m) {
 //		m.addAttribute("juego", service.findById(id));
 		return "formularioAlta";
 	}
-	
-	/** Metodo eliminarJuego() - permite eliminar un juego de la lista Juegos
-	* @param Eliminar juego
-	* @return /endpoint listaJuegos
-	* @author Lamia
-	* @version 1.0 23/09/22
-	*/
-	
+
+	/**
+	 * Metodo eliminarJuego() - permite eliminar un juego de la lista Juegos
+	 * 
+	 * @param Eliminar juego
+	 * @return /endpoint listaJuegos
+	 * @author Lamia
+	 * @version 1.0 23/09/22
+	 */
+
 	@GetMapping("/eliminarJuego")
-	public String eliminarJuego(@RequestParam("id")int id) {
+	public String eliminarJuego(@RequestParam("id") int id) {
 //		service.deleteById(id);
 		return ("redirect:/");
-		
+
 	}
 
 	/**
