1068c4e7c3b23b2e1837efc08b48c7311792d2cd
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2022-09-26 12:25:00 +0200

    UpdateJuegos.Dao

--- src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
+++ src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
@@ -8,36 +8,11 @@ import org.springframework.stereotype.Repository;
 
 import com.grupo2.lucasteam.model.Juego;
 
-/**
- * Interfaz JuegosDAOI que se encarga de gestionar las consultas a la tabla
- * Juegos de la BBDD.
- *
- * @author Álvaro Román Gómez
- * @version 1.0: 22/09/2022
- */
 @Repository
 public interface JuegosDAOI extends JpaRepository<Juego, Integer> {
 
-	/**
-	 * Consulta a la base de datos para encontrar un juego según el valor del campo
-	 * de nombre.
-	 * 
-	 * @param nombre - String con el nombre del juego
-	 * @return Optional<Juego>
-	 * @author Álvaro Román Gómez
-	 * @since 1.0
-	 */
 	Optional<Juego> findByNombre(String nombre);
 
-	/**
-	 * Consulta a la base de datos para encontrar todos los juegos según el valor
-	 * del campo de nombre.
-	 * 
-	 * @param nombre - String con el nombre del juego
-	 * @return ArrayList<Juego>
-	 * @author Álvaro Román Gómez
-	 * @since 1.0
-	 */
 	ArrayList<Juego> findAllByNombre(String nombre);
 	
 	ArrayList<Juego> findAllByPlataforma(String plataforma);
@@ -47,5 +22,6 @@ public interface JuegosDAOI extends JpaRepository<Juego, Integer> {
 	ArrayList<Juego> findAllByGenero(String genero);
 
 	ArrayList<Juego> findAllByFecha(int fecha);
+	
 	
 }
