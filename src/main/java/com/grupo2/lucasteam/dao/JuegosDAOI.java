ba1802bb062d4b581bfc613a4cd4bfba0fe22900
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2022-09-26 12:20:18 +0200

    Update JuegosDAOI.java

--- src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
+++ src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.grupo2.lucasteam.model.Juego;
@@ -14,5 +15,13 @@ public interface JuegosDAOI extends JpaRepository<Juego, Integer> {
 	Optional<Juego> findByNombre(String nombre);
 
 	ArrayList<Juego> findAllByNombre(String nombre);
+	
+	ArrayList<Juego> findAllByPlataforma(String plataforma);
+	
+	ArrayList<Juego> findAllByEditor(String editor);
 
+	ArrayList<Juego> findAllByGenero(String genero);
+
+	ArrayList<Juego> findAllOrderByFecha(int fecha);
+	
 }
