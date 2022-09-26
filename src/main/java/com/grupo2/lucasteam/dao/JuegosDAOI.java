7586a141b0e0014b8aefddf46557df3cffdd8141
Carlos Jesús Pérez Márquez <permarcar96@gmail.com>
2022-09-26 12:21:37 +0200

    Update JuegosDAO

--- src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
+++ src/main/java/com/grupo2/lucasteam/dao/JuegosDAOI.java
@@ -22,6 +22,6 @@ public interface JuegosDAOI extends JpaRepository<Juego, Integer> {
 
 	ArrayList<Juego> findAllByGenero(String genero);
 
-	ArrayList<Juego> findAllOrderByFecha(int fecha);
+	ArrayList<Juego> findAllByFecha(int fecha);
 	
 }
